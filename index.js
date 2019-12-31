91f1b9d69873416f4f53d8912bfd9cb35668aacd
= <zainr7989@gmail.com>
2019-12-31 18:35:19 -0500

    [Add] Import from another file.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+// try to print to the console
 console.log('Hello, and this is my first Node.js project!');
+
+// try to import from another folder in this project, using vanilla JavaScript
+const person = require('./person');
+console.log("My best friend is " + person.name);
---
+++ person.js
@@ -0,0 +1,8 @@
+// let's create an object literal that represents a person
+const person = {
+    'name': 'Bobby the Sealion',
+    'age': 76
+}
+
+// hey! let's export the object now:
+module.exports = person;
