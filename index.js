49d7d84e2b0b5df92755cb32500b5696ba2a8714
= <zainr7989@gmail.com>
2020-01-01 09:37:24 -0500

    [Add] Response to show About page.

--- index.js
+++ index.js
@@ -9,15 +9,30 @@ const server = http.createServer((req, res) => {
     console.log(req.url);
     // as a response, display some HTML
     if(req.url == '/') {
-        // response:
+        // response for home page
         fs.readFile(
-            path.join(__dirname, 'public', 'index.html'), (err, content) => {
+            path.join(__dirname, 'public', 'index.html'),
+            (err, content) => {
                 // error handling
                 if (err) throw err;
                 // display html, add HTTP header (status code and content type)
                 res.writeHead(200, {"Content-Type": 'text/html'});
                 res.end(content);
-        })
+            }
+        )
+    }
+
+    if (req.url == '/about') {
+        fs.readFile(
+            path.join(__dirname, 'public', 'about.html'),
+            (err, content) => {
+                // handle any errors (just like for home page)
+                if (err) throw err;
+                // display some HTML, add header (status code, content type)
+                res.writeHead(200, {"Content-Type": 'text/html'});
+                res.end(content);
+            }
+        )
     }
 });
 
