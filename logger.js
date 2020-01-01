0b228753a4e703cd385d47d82f29b0226d6fc0b7
= <zainr7989@gmail.com>
2020-01-01 08:30:40 -0500

    [Add] Logger class (practical example with EventEmitters).

---
+++ logger.js
@@ -0,0 +1,16 @@
+const EventEmitter = require('events');
+const uuid = require('uuid'); // a module for generating Universally Unique Ids
+
+// generate an id
+console.log(uuid.v4());
+
+// subclass a Logger class from EventEmitter
+class Logger extends EventEmitter {
+    log(msg) {
+        // Call event, emit a message containing an id
+        this.emit('message', { id: uuid.v4(), msg });
+    }
+}
+
+// export the Logger class
+module.exports = Logger;
